6e00777cb8fb44f76de03ed28b3d7800f4f45246
kannagi <fanskymisaki@gmail.com>
2017-03-25 18:28:23 +0800

    Rejected commit: Default

---
+++ src/com/fannagi/hello/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.fannagi.hello;
+
+/**
+ * Created by ME on 2017/3/8.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.printf("HelloWorld!");
+        System.out.printf("test");
+        System.out.printf("test1");
+    }
+}
